3ee503a6475937d2d15733e05f29de2e43758331
Carles Muiños <carles.ml.dev@gmail.com>
2017-02-07 18:07:43 +0100

    use firebase as the backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+import { firebaseConfig } from '../config/firebase';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
