e1570f07aaa65bb924da1787d4b2f029503b860b
Plastikaweb <info@plastikaweb.com>
2017-02-15 12:41:38 +0100

    feat(geocoding): mapzen-geocode.service getGeocodng && geocode.service getCoords methods served

--- src/app/services/geocode.service.ts
+++ src/app/services/geocode.service.ts
@@ -1,17 +1,23 @@
 import {Injectable, ComponentFactoryResolver} from '@angular/core';
 import {MapzenGeocodeService} from './mapzen-geocode.service';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class GeocodeService {
 
-  constructor(private mapzen: MapzenGeocodeService) { }
+  constructor(private geocodeService: MapzenGeocodeService) { }
 
-  getCoords (address: string) {
-    this.mapzen.geocoding(address);
+  /**
+   * Returns coords [long, lat]
+   * @param address
+   * @returns {Observable<number[]>}
+   */
+  getCoords (address: string): Observable<number[]> {
+    return this.geocodeService.getGeocoding(address);
   }
 
   getAddress (lat: number, long: number) {
-
+    // return this.geocodeService.getReverseGeocoding(lat, long);
   }
 
 }
--- src/app/services/mapzen-geocode.service.ts
+++ src/app/services/mapzen-geocode.service.ts
@@ -1,21 +1,56 @@
 import {Injectable} from '@angular/core';
-import {Geocode} from './geocode';
-import {Http} from '@angular/http';
+import {Geocode, MAPZEN_API_KEY} from './geocode';
+import {Http, URLSearchParams, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class MapzenGeocodeService implements Geocode {
 
   baseUrl = 'https://search.mapzen.com/v1/search';
+  private searchParams = {
+    'boundary.country': 'ES',
+    'sources': 'oa',
+    'layers': 'address',
+    'size': '1',
+    'api_key': MAPZEN_API_KEY
+  };
 
   constructor(private http: Http) {
   }
 
-  geocoding(address: string) {
+  /**
+   * Returns coords [long, lat]
+   * @param address
+   * @returns {Observable<number[]>}
+   */
+  getGeocoding(address: string): Observable<number[]> {
+    const params: URLSearchParams = new URLSearchParams();
+    for (const key in this.searchParams) {
+      if (this.searchParams.hasOwnProperty(key)) {
+        const val = this.searchParams[key];
+        params.set(key, val);
+      }
+    }
+    params.set('text', address);
+
+    return this.http.get(this.baseUrl, {
+      search: params
+    }).map(
+      (response: Response) => {
+        return response.json().features[0].geometry.coordinates;
+      }
+    )
+      .catch(this.handleError);
+  }
+
+  getReverseGeocoding(lat: number, long: number) {
 
   }
 
-  reverseGeocoding(lat: number, long: number) {
-
+  handleError(error: any) {
+    const errResponse = error.json().geocoding;
+    const errMsg = errResponse.errors ? errResponse.errors[0] : 'Mapzen search server error';
+    return Observable.throw(errMsg);
   }
 
 }
