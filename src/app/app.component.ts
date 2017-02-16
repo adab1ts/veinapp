530758bebe5994778e9eca303c95d1cdf360dadc
Carles Muiños <carles.ml.dev@gmail.com>
2017-02-16 01:39:49 +0100

    build: fix testing setup
    Remove unnecessary testing dependencies and refactor testing setup accordingly.
    
    Close #13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import {Component, OnInit} from '@angular/core';
+import { GeocodeService } from './geo/geo.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  coords: number[] = [];
 
-  constructor(private af: AngularFire) {
-    console.log(af);
+  constructor(private geocodeService: GeocodeService) {
   }
+
+  ngOnInit() {
+    this.geocodeService.getCoords('Carrer de la Jota 55, Barcelona')
+      .subscribe(
+        data => this.coords = data,
+        error => console.error(error)
+      );
+  }
+
 }
