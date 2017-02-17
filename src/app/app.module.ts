3896e7e3417cbacb4d199804da8435922d7a28aa
Plastikaweb <info@plastikaweb.com>
2017-02-17 18:36:20 +0100

    geo.module development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { AngularFireModule } from 'angularfire2';
 import { CovalentCoreModule } from '@covalent/core';
 
 import './rxjs-extensions';
 
-import {ServicesModule} from './services/services.module';
 import { AppComponent } from './app.component';
 import { firebaseConfig } from '../config/firebase';
+import { GeoModule } from './geo/geo.module';
 
 @NgModule({
   declarations: [
@@ -18,10 +17,9 @@ import { firebaseConfig } from '../config/firebase';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    ServicesModule.forRoot(),
-    CovalentCoreModule.forRoot()
+    CovalentCoreModule.forRoot(),
+    GeoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
