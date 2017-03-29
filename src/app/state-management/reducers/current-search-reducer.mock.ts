2e426213977c3686e03e3928585b3ced86a04b39
Plastikaweb <info@plastikaweb.com>
2017-03-29 16:32:59 +0200

    test(testing): current search reducer unit tests

---
+++ src/app/state-management/reducers/current-search-reducer.mock.ts
@@ -0,0 +1,31 @@
+export const mockState = {
+  geocodeData: {
+    address: `Mocked St. 111, Barcelona`,
+    center: [ 41.3874990, 2.1625900 ],
+    radius: 1
+  },
+  selectedPlace: null,
+  placesList: [
+    {
+      name: 'AV xxx Barcelona',
+      $key: '-KfVdriJAlARLEO2N2l_'
+    },
+    {
+      name: 'AV yyy Barcelona',
+      $key: '-KfVdrh0bso_sI8cvFYx'
+    },
+    {
+      name: 'AV zzz Barcelona',
+      $key: '-KfVdrj7PzN3qN-tKY6m'
+    },
+    {
+      name: 'AV rrr Barcelona',
+      $key: '-KfVdriZoQXLDxFWaeqP'
+    },
+    {
+      name: 'AV kkk Barcelona',
+      $key: '-KfVdridMr2TGAfOSyng'
+    }
+  ],
+  pending: false
+};
--- src/app/state-management/reducers/current-search-reducer.spec.ts
+++ src/app/state-management/reducers/current-search-reducer.spec.ts
@@ -1,38 +1,143 @@
 /* tslint:disable:no-unused-variable */
-
 import { INITIAL_CURRENT_SEARCH_STATE } from '../states/current-search-state';
 import { ActionTypes } from '../actions/current-search-action';
-import { reducer } from './current-search-reducer';
+import {
+  reducer, getRadius, getCenter, getAddress, getPlacesList, getPending,
+  getSelectedPlace
+} from './current-search-reducer';
+import { mockState } from './current-search-reducer.mock';
 
-describe('The Current Search Reducer', () => {
+describe('Current Search Reducer Actions', () => {
 
-  it('should handle initial current search state', () => {
-    const actual = reducer(INITIAL_CURRENT_SEARCH_STATE, {type: 'INIT_STATE'});
-
-    expect(actual.geocodeData.address).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.address);
-    expect(actual.geocodeData.center[0]).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.center[0]);
-    expect(actual.geocodeData.center[1]).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.center[1]);
+  it('should handle the state when a pending geo search is in process', () => {
+    const newState = reducer(mockState, {
+      type: ActionTypes.DO_GEO_SEARCH
+    });
+    expect(newState).not.toEqual(mockState);
+    expect(newState.pending).toBeTruthy();
   });
 
-  it('should handle a new state when we invoke a new address', () => {
-    const state = {
-      geocodeData: {
-        address: `Carrer d'Enric Granados 5, Barcelona`,
-        center: [41.3874990, 2.1625900],
-        radius: 1
-      },
-      placesList: [],
-      pending: false
-    };
+  it('should handle the state when a pending change radius search is in process', () => {
+    const newState = reducer(mockState, {
+      type: ActionTypes.CHANGE_SEARCH_BY_RADIUS
+    });
+    expect(newState).not.toEqual(mockState);
+    expect(newState.pending).toBeTruthy();
+  });
 
-    const actual = reducer(state, {
+  it('should handle the state when a pending address change search is in process', () => {
+    const newState = reducer(mockState, {
+      type: ActionTypes.CHANGE_SEARCH_FROM_ADDRESS
+    });
+    expect(newState).not.toEqual(mockState);
+    expect(newState.pending).toBeTruthy();
+  });
+
+  it('should handle a search state with initial state values', () => {
+    const newState = reducer(INITIAL_CURRENT_SEARCH_STATE, { type: 'INIT_STATE' });
+
+    expect(newState).not.toEqual(mockState);
+    expect(newState.geocodeData.address).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.address);
+    expect(newState.geocodeData.center[ 0 ]).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.center[ 0 ]);
+    expect(newState.geocodeData.center[ 1 ]).toEqual(INITIAL_CURRENT_SEARCH_STATE.geocodeData.center[ 1 ]);
+  });
+
+  it('should handle a new search state when we invoke a new center', () => {
+    const newPayload = {
+      address: `Street Mocked 333, Barcelona`,
+      center: [ 42.523242, 2.00042 ],
+    };
+    const newState = reducer(mockState, {
       type: ActionTypes.CHANGE_CURRENT_PARAMS,
-      payload: state
+      payload: newPayload
     });
 
-    expect(actual.geocodeData.address).toEqual(state.geocodeData.address);
-    expect(actual.geocodeData.center).toEqual(state.geocodeData.center);
-
+    expect(newState).not.toEqual(mockState);
+    expect(newState.geocodeData.address).toEqual(newPayload.address);
+    expect(newState.geocodeData.center).toEqual(newPayload.center);
   });
 
+  it('should handle a new search state when we invoke a new radius', () => {
+    const newPayload = {
+      radius: 2
+    };
+    const newState = reducer(mockState, {
+      type: ActionTypes.CHANGE_CURRENT_PARAMS,
+      payload: newPayload
+    });
+
+    expect(newState).not.toEqual(mockState);
+    expect(newState.geocodeData.radius).toEqual(newPayload.radius);
+  });
+
+  it('should handle a new placesList property when new geo search results are available', () => {
+    const newPayload = [
+      { $key: '-KfVdriJAlARLEO2N2l_', remove: true },
+      { $key: '-KfVdrh0bso_sI8cvFYx', remove: true },
+      { name: 'AV qqq Barcelona', $key: '-GHRDdeET3gW-Klt2fm' }
+    ];
+
+    const newState = reducer(mockState, {
+      type: ActionTypes.UPDATE_GEOSEARCH_RESULTS,
+      payload: newPayload
+    });
+    expect(newState).not.toEqual(mockState);
+    expect(newState.placesList.length).toEqual(4);
+  });
+
+  it('should handle when no geo search results are provided, not changing the state', () => {
+    const newState = reducer(mockState, {
+      type: ActionTypes.NO_RESULTS_SEARCH
+    });
+    expect(newState).toEqual(mockState);
+  });
+
+  it('should handle a new selected place property when a place is selected or deselected', () => {
+    const newPayload = {
+        name: 'AV xxx Barcelona',
+        $key: '-KfVdriJAlARLEO2N2l_'
+      };
+
+    const newState = reducer(mockState, {
+      type: ActionTypes.SELECTED_PLACE,
+      payload: newPayload
+    });
+    expect(newState).not.toEqual(mockState);
+    expect(newState.selectedPlace).not.toBeNull();
+    expect(newState.selectedPlace.$key).toEqual(newPayload.$key);
+
+    const newState2 = reducer(mockState, {
+      type: ActionTypes.SELECTED_PLACE,
+      payload: null
+    });
+    expect(newState2).not.toEqual(newState);
+    expect(newState2.selectedPlace).toBeNull();
+  });
+});
+
+describe('Current Search Reducer Selectors', () => {
+
+  it('should get the current search geocodeData radius value', () => {
+    expect(getRadius(mockState)).toEqual(1);
+  });
+
+  it('should get the current search geocodeData center value', () => {
+    expect(getCenter(mockState)).toEqual([ 41.3874990, 2.1625900 ]);
+  });
+
+  it('should get the current search geocodeData address value', () => {
+    expect(getAddress(mockState)).toEqual(`Mocked St. 111, Barcelona`);
+  });
+
+  it('should get the current search placesList value', () => {
+    expect(getPlacesList(mockState).length).toEqual(5);
+  });
+
+  it('should get the current search pending value', () => {
+    expect(getPending(mockState)).toBeFalsy();
+  });
+
+  it('should get the current search selectedPlace value', () => {
+    expect(getSelectedPlace(mockState)).toBeNull();
+  });
 });
