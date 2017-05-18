148f54ae5a422be84e04d2087f0b27e1b9c09071
Carles Muiños <carles.ml.dev@gmail.com>
2017-05-18 19:24:01 +0200

    refactor(ui): reorganize map's moving elements
    Remove attribution to reduce friction in mobile view and move zoom control position.

--- src/app/geo/map/leaflet-map/leaflet-config.ts
+++ src/app/geo/map/leaflet-map/leaflet-config.ts
@@ -3,7 +3,8 @@ import ZoomOptions = L.Control.ZoomOptions;
 
 export class LeafletConfig {
   static INIT_ZOOM = 14;
-  static CONTROL_ZOOM_POSITION: ZoomOptions = { position: 'topright' };
+  static CONTROL_ZOOM_POSITION: ZoomOptions = { position: 'bottomright' };
+
   private static MARKERS_PATH = '/assets/leaflet-icons';
   private static PlaceIcon = Icon.extend({
     options: {
@@ -11,22 +12,22 @@ export class LeafletConfig {
       iconAnchor: [ 0, 12 ]
     }
   });
+
   static CENTER_MARKER = new LeafletConfig.PlaceIcon({
     iconUrl: `${LeafletConfig.MARKERS_PATH}/centre-25x25.png`
   });
+
   static PLACE_MARKER = new LeafletConfig.PlaceIcon({
     iconUrl: `${LeafletConfig.MARKERS_PATH}/casa-25x25.png`
   });
+
   static SELECTED_MARKER = new LeafletConfig.PlaceIcon({
     iconUrl: `${LeafletConfig.MARKERS_PATH}/pointer-35x35.png`,
     iconSize: [ 35, 35 ],
     iconAnchor: [ 0, 17 ]
   });
+
   static BASE_MAPS = {
-    OpenStreetMap: tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, 
-                      Tiles courtesy of <a href="https://www.hotosm.org/" target="_blank">
-                      Humanitarian OpenStreetMap Team</a>`
-    })
+    OpenStreetMap: tileLayer('https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png')
   };
 }
