3d1056b9a11e1e05d428b8f6b666a4511a9bbc44
Carles Muiños <carles.ml.dev@gmail.com>
2017-05-18 20:10:17 +0200

    refactor(ui): link brand name to website

--- src/app/shared/branding/branding.component.ts
+++ src/app/shared/branding/branding.component.ts
@@ -3,15 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-branding',
   template: `
-    <div class="va-branding">{{ brand }}</div>
+    <div class="va-branding">{{ title }} | <a href="{{ url }}" target="_blank">{{ brand }}</a></div>
   `,
   styles: [`
     .va-branding {
       padding-right: 0.5rem;
       text-align: right;
     }
+
+    .va-branding a {
+      text-decoration: none;
+      color: white;
+    }
   `]
 })
 export class BrandingComponent {
-  brand = `Cercador d'AV | CONFAVC`;
+  title = `Cercador d'AV`;
+  brand = 'CONFAVC';
+  url = 'http://confavc.cat';
 }
