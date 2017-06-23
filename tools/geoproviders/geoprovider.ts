716a22ccd209fa8736bec59d8bd574097f581a3d
Carles Muiños <carles.ml.dev@gmail.com>
2017-06-23 13:08:40 +0200

    chore(tools): add tools to consume geoservices
    A geoprovider exposes geocoding capabilities.

---
+++ tools/geoproviders/geoprovider.ts
@@ -0,0 +1,73 @@
+
+import * as request from 'request-promise-native';
+
+import { Formatter } from '../../tools';
+
+/**
+ * Initialize configuration of geocoding service provider
+ * @param prod Whether to use production or development config
+ */
+function geoserviceConfig(prod: boolean): any {
+  let config = undefined;
+
+  if (prod) {
+    config = require('../../src/config/mapzen.prod');
+  } else {
+    config = require('../../src/config/mapzen');
+  }
+
+  const { mapzenConfig } = config;
+
+  return {
+    searchURL: `${mapzenConfig.apiURL}/search`,
+    reverseURL: `${mapzenConfig.apiURL}/reverse`,
+    searchParams: {
+      'api_key': mapzenConfig.apiKey,
+      'boundary.country': 'ES',
+      'sources': 'oa',
+      'layers': 'address',
+      'size': '1'
+    }
+  };
+}
+
+/**
+ * Geocode a place
+ * @param place      Place to geocode
+ * @param delta      Correction parameter to defer geocoding request
+ * @param geoservice Geocodind service provider configuration
+ */
+function geocodePlace(place: any, delta: number, geoservice: any): Promise<any> {
+  const formattedPlace = Formatter.format([{fn: Formatter.geoformat, keys: ['address']}])(place);
+
+  const searchTerm = `${formattedPlace.address}, ${formattedPlace.city}`;
+  const query = Object.assign({}, geoservice.searchParams, { 'text': searchTerm });
+  const opts = {
+    uri: geoservice.searchURL,
+    qs: query,
+    json: true
+  };
+
+  const geocoded = new Promise<any>((resolve, reject) => {
+    setTimeout(function() {
+      request(opts)
+        .then(response => {
+          const result = response.features[0];
+          const coords = result['geometry']['coordinates']; // [long, lat]
+          const geoplace = Object.assign({}, place, { latitude: coords[1], longitude: coords[0] });
+
+          console.log(`[${geoplace.latitude}, ${geoplace.longitude}] '${searchTerm}' => '${geoplace.name}'`);
+          resolve(geoplace);
+        })
+        .catch(err => {
+          console.error(`Failed to geocode location of '${place.name}'.`);
+          console.error('Error:', err.message);
+          reject(err);
+        });
+    }, delta * 250);
+  });
+
+  return geocoded;
+}
+
+export { geoserviceConfig, geocodePlace };
---
+++ tools/geoproviders/index.ts
@@ -0,0 +1,2 @@
+
+export { geoserviceConfig, geocodePlace } from './geoprovider';
