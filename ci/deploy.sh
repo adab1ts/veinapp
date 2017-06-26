c86c0e812e4faee0123f6cc29013a08a3bcb169e
Carles Muiños <carles.ml.dev@gmail.com>
2017-06-26 13:46:19 +0200

    fix(build): fix ProgressBar issue on firebase deploy
    See
    https://stackoverflow.com/questions/44736576/travis-with-firebase-deploy-typeerror-this-stream-clearline-is-not-a-function
    and apply firebase/firebase-tools#73

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,10 +3,10 @@
 set -ev
 
 if [ "${TRAVIS_BRANCH}" = "staging" ]; then
-    firebase deploy --project $FB_STAGING_PROJECT --token $FB_PROD_TOKEN
+    firebase deploy --project $FB_STAGING_PROJECT --token $FB_PROD_TOKEN --non-interactive
 
 elif [ "${TRAVIS_BRANCH}" = "master" ]; then
-    firebase deploy --project $FB_PROD_PROJECT --token $FB_PROD_TOKEN
+    firebase deploy --project $FB_PROD_PROJECT --token $FB_PROD_TOKEN --non-interactive
 fi
 
 exit 0
